3531bec7e351c547c9e910b4f88760be4024aaa4
marellanorero <marellanonorero@gmail.com>
2024-09-14 13:13:36 -0300

    ultimos detalles en sección 7

--- wp-content/themes/gymfitness/page-contenido-centrado.php
+++ wp-content/themes/gymfitness/page-contenido-centrado.php
@@ -1,8 +1,12 @@
 <?php 
+
+/**
+ * Template Name: Contenido Centrado (No Sidebar)
+ */
     get_header();
 ?>
 
-    <main class="contenedor seccion">
+    <main class="contenedor seccion contenido-centrado">
         <?php
             get_template_part('template-parts/pagina');
         ?>
